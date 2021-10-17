57eab4362c05816f0479892a158e6711f8bcaee1
Azman Abdullah <azmanabdlh@yahoo.com>
2021-10-17 13:54:49 +0900

    add carbon global helper

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,8 +1,22 @@
 <?php
 
+use DateTimeInterface;
 use Illuminate\Support\Str;
-use \Illuminate\Support\Stringable;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Stringable;
 
-function str($s): Stringable {
-    return Str::of($s);
+
+if (!function_exists('str')) {
+    function str($s): Stringable {
+        return Str::of($s);
+    }
+}
+
+
+if (!function_exists('carbon')) {
+    function carbon(string|DateTimeInterface|null $datetime): self {
+        if (is_null($datetime) || str($datetime)->isEmpty()) return Carbon::now();
+
+        return Carbon::parse($datetime);
+    }
 }
